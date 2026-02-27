5f5a04878464b6555a19db34e3449c5b37d62609
jorgelm95 <jorgeluis.m95@gmail.com>
2026-02-27 17:07:33 -0500

    release SharedLibraryTests to 1.0.0 version

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "SharedLibraryTests",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "SharedLibraryTests",
+            targets: ["SharedLibraryTests"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SharedLibraryTests",
+            url: "https://github.com/jorgelm95/KMMSharedLibSPMTest.git/releases/download/1.0.0/SharedLibraryTests.xcframework.zip",
+            checksum: "919219db904528294be30a82f546237b58ad5dcec008069da640dc75378028c5"
+        )
+    ]
+)
